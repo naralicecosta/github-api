496ffc9589552400b0bb4646d0dbbe79dd75c745
naralicecosta <naralicecosta04@gmail.com>
2024-05-17 22:13:29 -0300

    feat: :sparkles: busca de usuário

--- github_finder/src/components/Search.tsx
+++ github_finder/src/components/Search.tsx
@@ -1,13 +1,31 @@
 import { BsSearch } from 'react-icons/bs'
+import { useState, KeyboardEvent } from 'react'
+import classes from './Search.module.css'
 
-const Search = () => {
+type SearchProps = {
+    loadUser: (userName: string) => Promise<void>
+}
+
+const Search = ({loadUser}: SearchProps) => {
+    const [userName, setUserName] = useState("")
+
+    const handleKeyDown = (e: KeyboardEvent) => {
+        if(e.key === "Enter"){
+            loadUser(userName)
+        }
+    }
+
     return (
-        <div>
+        <div className={classes.search}>
             <h2>Busque por um usuário</h2>
             <p>Conheça seus melhores repositórios</p>
-            <div>
-                <input type="text" placeholder="Digite o nome do usuário" />
-                <button>
+            <div className={classes.search_container}>
+                <input 
+                    type="text" 
+                    placeholder="Digite o nome do usuário" 
+                    onChange={(e)=> setUserName(e.target.value)}
+                    onKeyDown={handleKeyDown}/>
+                <button onClick={()=> loadUser(userName)}>
                     <BsSearch />
                 </button>
             </div>
